99f82a417471f0dee1f7969cde631d7eb73ff1bc
Xin Lei Lin <xinlei55555@gmail.com>
2025-03-31 16:53:42 -0400

    feat: validation and testing set for sklearn only model

--- train_naive_bayes_np_only.py
+++ train_naive_bayes_np_only.py
@@ -1,26 +1,3 @@
-# ------------------- Original Code with sklearn ----------------
-# from models.naive_bayes import train_naive_bayes, make_inference
-# from sklearn.feature_extraction.text import CountVectorizer
-# import pandas as pd
-
-# data_path = 'data/cleaned_data_combined_modified.csv'
-# model = train_naive_bayes(data_path)
-
-# # Create a CountVectorizer instance
-# vectorizer = CountVectorizer()
-
-# # Fit the vectorizer on the training data
-# data = pd.read_csv(data_path, delimiter=',', quotechar='"')
-# X = data.iloc[:, :-1]
-# X = X.fillna('')
-# vectorizer.fit(X.apply(lambda row: ' '.join(row.values.astype(str)), axis=1))
-# # accuracy 0.86
-
-# inference = make_inference(
-#     model, vectorizer, 'I love eating this product on Saturday evening with 3-4 ingredients, and I have little hot sauce')
-# print(f'Inference: {inference}')
-# ------------------- Original Code with sklearn ----------------
-
 # -------------------- New code with Numpy only ---------------
 import csv
 import random
