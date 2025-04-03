dc92aa752dda2305e39148a9a1f9028c2b7e3083
Xin Lei Lin <xinlei55555@gmail.com>
2025-04-02 20:52:44 -0400

    feat: new prediction script

--- final_submission_markus/pred.py
+++ final_submission_markus/pred.py
@@ -9,7 +9,6 @@ import json
 
 
 def load_data(data_path):
-
     with open(data_path, newline='', encoding='utf-8') as f:
         reader = csv.reader(f)
         data = [row for row in reader]
@@ -213,9 +212,10 @@ def predict_all(csv_name):
 if __name__ == '__main__':
     # Example usage
     # csv_name = os.path.join('test_data', 'data_without_labels.csv')
+    # csv_name = os.path.join('test_data_more', 'test.csv')
     # predictions = predict_all(csv_name)
 
-    # labels_df = pd.read_csv(os.path.join('test_data', "labels.csv"), header=None)
+    # labels_df = pd.read_csv(os.path.join('test_data_more', "test_targets.csv"), header=None)
 
     # # Display the loaded labels
     # print(labels_df)
